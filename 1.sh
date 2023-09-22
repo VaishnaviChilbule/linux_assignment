87fce88b2cec7c797ad79072b9fc97e69f87e47b
Ubuntu <ubuntu@ip-172-31-12-171.ap-south-1.compute.internal>
2023-09-22 01:23:42 +0000

    new file

---
+++ 1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "you are logged in $USER"
+echo "present working directory is:-"
+pwd
+echo "your working terminal is $TERM"
+echo "total number of files and directories are:-"
+ls -l | wc
